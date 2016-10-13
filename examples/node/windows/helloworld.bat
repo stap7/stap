9eb92aa5d7486503789cd281155a4a62c17ce2c6
V.D. Veksler <vdv718@gmail.com>
2016-10-13 15:17:40 -0400

    error fixed in helloworld.bat

--- examples/node/windows/helloworld.bat
+++ examples/node/windows/helloworld.bat
@@ -1,3 +1,3 @@
-start node1.exe ..\..\taskServer\stdio2socketServ.js "python ..\..\tasks\helloworld.py" -w 8800 -h "..\..\taskServer\helloworld.html"
+start node.exe ..\..\taskServer\stdio2socketServ.js "python ..\..\tasks\helloworld.py" -w 8800 -h "..\..\taskServer\helloworld.html"
 timeout 3
 start ..\..\taskServer\helloworld.html
